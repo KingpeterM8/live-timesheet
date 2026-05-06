d11d4d054930d01a2e22c4d3424514604eb786cb
KingpeterM8 <64614269+KingpeterM8@users.noreply.github.com>
2026-05-06 14:18:10 -0700

    First commit

--- empyrealtimesheet/settings.py
+++ empyrealtimesheet/settings.py
@@ -7,7 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/6.0/topics/settings/
 
 For the full list of settings and their values, see
-https://docs.djangoproject.com/en/6.0/ref/settings/
+https://docs.djangoproject.com/en/6.0/ref/settings/ dada
 """
 
 from pathlib import Path
